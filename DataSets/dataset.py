9c4ee390a37efb7bf5ac561c11b95bb3d45f12dc
Bruce Lau <brucelau.email@gmail.com>
2017-02-03 18:57:01 -0500

    SuperVidsed learning

--- DataSets/dataset.py
+++ DataSets/dataset.py
@@ -12,3 +12,11 @@ plt.imshow(digits.images[-1], cmap=plt.cm.gray_r)
 
 data = digits.images.reshape((digits.images.shape[0], -1))
 print(data)
+
+# estimator.fit(data)
+# estimator = Estimator(param1=1, param2=2)
+# estimator.param1
+# estimator.estimated_param_
+# When data is fitted with an estimator, parameters are estimated from
+# the data at hand. All the estimated parameters are attributes of the
+#  estimator object ending by an underscore:
